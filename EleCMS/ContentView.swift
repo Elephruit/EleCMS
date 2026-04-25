16b9e4ebf23e2e4c0699418779a17bd3dc9ddbfe
Elephruit <mike@emzpc.com>
2026-04-25 15:54:52 -0500

    Initial Commit

---
+++ EleCMS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EleCMS
+//
+//  Created by M Zehrer on 4/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EleCMS/EleCMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EleCMSApp.swift
+//  EleCMS
+//
+//  Created by M Zehrer on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct EleCMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
